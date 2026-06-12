0dcb598a5d04eae62616569605dfb77d51fe923c
Nathan Mendonça <nthn.mdc@gmail.com>
2026-06-12 08:53:18 -0300

    feat: add /register route to auth controller for admin creation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,5 +4,7 @@ const authController = require('../controllers/authController')
 const router = Router()
 
 router.post('/login', authController.login)
+router.post('/register', authController.createAdmin)
 
 module.exports = router
+
